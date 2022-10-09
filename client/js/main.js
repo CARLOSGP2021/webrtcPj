c336bc602259963a0d7a7cc3902cac0f1dd19d37
CARLOSGP2021 <carlosguo@foxmail.com>
2022-10-09 17:51:35 +0800

    10

--- client/js/main.js
+++ client/js/main.js
@@ -1,118 +1,271 @@
 'use strict';
 
-// join主动加入房问
-// leave主动离开房问
-// new-peer有人加入房间，通知已经在房间的人
-// peer-leave 有人离I房间，通知已经在房间的人
-// offer 送offer对端peer
-// answer送offer对端peer
-// candidate 送candidate端peer
+// join 主动加入房间
+// leave 主动离开房间
+// new-peer 有人加入房间，通知已经在房间的人
+// peer-leave 有人离开房间，通知已经在房间的人
+// offer 发送offer给对端peer
+// answer发送offer给对端peer
+// candidate 发送candidate给对端peer
 const SIGNAL_TYPE_JOIN = "join";
-const SIGNAL_TYPE_RESP_JOIN = "resp-join"; // 告知加入者对方是谁
-const SIGNAL_IYPE_LEAVE = "leave";
-const SIGNAL_TYPE_NEW_PEER ="new-peer";
+const SIGNAL_TYPE_RESP_JOIN = "resp-join";  // 告知加入者对方是谁
+const SIGNAL_TYPE_LEAVE = "leave";
+const SIGNAL_TYPE_NEW_PEER = "new-peer";
 const SIGNAL_TYPE_PEER_LEAVE = "peer-leave";
 const SIGNAL_TYPE_OFFER = "offer";
 const SIGNAL_TYPE_ANSWER = "answer";
 const SIGNAL_TYPE_CANDIDATE = "candidate";
 
-var localUserID = Math.random().toString(36).substring(2); //本地ID
-var remoteUserID = -1; //对端
+
+var localUserId = Math.random().toString(36).substr(2); // 本地uid
+var remoteUserId = -1;      // 对端
+var roomId = 0;
 
 var localVideo = document.querySelector('#localVideo');
 var remoteVideo = document.querySelector('#remoteVideo');
-var loaclStream = null;
-var ZeroRTCEngine;
+var localStream = null;
+var remoteStream = null;
+var pc = null;
 
+var zeroRTCEngine;
 
-var ZeroRTCEngine = function(wsURL){
-    ZeroRTCEngine = this;
+function handleIceCandidate(event) {
+    console.info("handleIceCandidate");
+    if (event.candidate) {
+        var jsonMsg = {
+            'cmd': 'candidate',
+            'roomId': roomId,
+            'uid': localUserId,
+            'remoteUid': remoteUserId,
+            'msg': JSON.stringify(event.candidate)
+        };
+        var message = JSON.stringify(jsonMsg);
+        zeroRTCEngine.sendMessage(message);
+        // console.info("handleIceCandidate message: " + message);
+        console.info("send candidate message");
+    } else {
+        console.warn("End of candidates");
+    }
+}
+
+function handleRemoteStreamAdd(event){
+    console.info("handleRemoteStreamAdd");
+    remoteStream = event.streams[0];
+    remoteVideo.srcObject = remoteStream;
+}
+
+function createPeerConnection() {
+    var defaultConfiguration = {
+        bundlePolicy: "max-bundle",
+        rtcpMuxPolicy: "require",
+        iceTransportPolicy:"relay",//relay 或者 all
+        // 修改ice数组测试效果，需要进行封装
+        iceServers: [
+            {
+                "urls": [
+                    "turn:公网ip/域名?transport=udp",
+                    "turn:公网ip/域名?transport=tcp"       // 可以插入多个进行备选
+                ],
+                "username": "test",  // coturn 设置的
+                "credential": "123456"
+            },
+            {
+                "urls": [
+                    "stun:公网ip/域名"
+                ]
+            }
+        ]
+    };
+ 
+    pc = new RTCPeerConnection(defaultConfiguration);
+    pc.onicecandidate = handleIceCandidate;  //回调Candidate 函数
+    pc.ontrack = handleRemoteStreamAdd;		//回调处理远端流的函数
+    pc.onconnectionstatechange = handleConnectionStateChange;
+    pc.oniceconnectionstatechange = handleIceConnectionStateChange
+
+    localStream.getTracks().forEach((track) => pc.addTrack(track, localStream)); // 给video本地的绑定本地流 中的对应轨  WebRtc不同于ffmpeg一流可以多轨  一轨== 一个音视频ffmpeg流那种
+}
+
+function createOfferAndSendMessage(session) {
+    pc.setLocalDescription(session)
+        .then(function () {
+            var jsonMsg = {
+                'cmd': 'offer',
+                'roomId': roomId,
+                'uid': localUserId,
+                'remoteUid': remoteUserId,
+                'msg': JSON.stringify(session)
+            };
+            var message = JSON.stringify(jsonMsg);
+            zeroRTCEngine.sendMessage(message);
+            // console.info("send offer message: " + message);
+            console.info("send offer message");
+        })
+        .catch(function (error) {
+            console.error("offer setLocalDescription failed: " + error);
+        });
+}
+
+function handleCreateOfferError(){
+    console.error("handleCreateOfferError: " + error);    
+}
+
+var ZeroRTCEngine = function(wsUrl) {
+    this.init(wsUrl);
+    zeroRTCEngine = this;
     return this;
 }
 
-ZeroRTCEngine.prototype.init = function(wsURL){
-    //设置websocket URL
-    this.wsURL = wsURL;
-    //websocket对象
+ZeroRTCEngine.prototype.init = function(wsUrl) {
+    // 设置websocket  url
+    this.wsUrl = wsUrl;
+    /** websocket对象 */
     this.signaling = null;
 }
 
-ZeroRTCEngine.prototype.createWebsocket = function(){
-    ZeroRTCEngine = this;
-    ZeroRTCEngine.signaling = new WebSocket(this.wsURL);
-    ZeroRTCEngine.signaling.onOpen = function(){
-        ZeroRTCEngine.onOpen();
-    }
-    ZeroRTCEngine.signaling.onMessage = function(event){
-        ZeroRTCEngine.onMessage(event);
-    }
-    ZeroRTCEngine.signaling.onError = function(event){
-        ZeroRTCEngine.onError(event);
-    }
-    ZeroRTCEngine.signaling.onClose = function(event){
-        ZeroRTCEngine.onClose(event);
+ZeroRTCEngine.prototype.createWebsocket = function() {
+    zeroRTCEngine = this;
+    zeroRTCEngine.signaling = new WebSocket(this.wsUrl);
+
+    zeroRTCEngine.signaling.onopen = function() {
+        zeroRTCEngine.onOpen();
     }
 
+    zeroRTCEngine.signaling.onmessage = function(ev) {
+        zeroRTCEngine.onMessage(ev);
+    }
+
+    zeroRTCEngine.signaling.onerror = function(ev) {
+        zeroRTCEngine.onError(ev);
+    }
+
+    zeroRTCEngine.signaling.onclose = function(ev) {
+        zeroRTCEngine.onClose(ev);
+    }
 }
 
-ZeroRTCEngine.prototype.onOpen = function(){
+ZeroRTCEngine.prototype.onOpen = function() {
     console.log("websocket open");
 }
-
-ZeroRTCEngine.prototype.onMessage = function(event){
+ZeroRTCEngine.prototype.onMessage = function(event) {
     console.log("onMessage: " + event.data);
+
+    var jsonMsg = JSON.parse(event.data);
+    switch(jsonMsg.cmd) {
+        case SIGNAL_TYPE_NEW_PEER:
+            handleRemoteNewPeer(jsonMsg);
+            break;
+        case SIGNAL_TYPE_RESP_JOIN:
+            handleResponseJoin(jsonMsg);
+            break;
+        case SIGNAL_TYPE_PEER_LEAVE:
+            handleRemotePeerLeave(jsonMsg);
+            break;   
+        case SIGNAL_TYPE_OFFER:
+            handleRemoteOffer(jsonMsg);
+            break;
+        case SIGNAL_TYPE_ANSWER:
+            handleRemoteAnswer(jsonMsg);
+            break;
+        case SIGNAL_TYPE_CANDIDATE:
+            handleRemoteCandidate(jsonMsg);
+            break;
+        }
 }
 
-ZeroRTCEngine.prototype.onError = function(event){
+ZeroRTCEngine.prototype.onError = function(event) {
     console.log("onError: " + event.data);
 }
 
-ZeroRTCEngine.prototype.onClose = function(event){
-    console.log("onClose: " + event.code + ", reason: " + EventTarget.reason);
-}
-ZeroRTCEngine.prototype.sendMsg = function(msg){
-    this.signaling.send(msg);
+ZeroRTCEngine.prototype.onClose = function(event) {
+    console.log("onClose -> code: " + event.code + ", reason:" + EventTarget.reason);
 }
 
+ZeroRTCEngine.prototype.sendMessage = function(message) {
+    this.signaling.send(message);
+}
 
-function doJoin(roomID){
-    var jsonMsg = {
-            'cmd': 'join',
-            'roomId': roomID,
-            'uid': localUserID,
+function handleResponseJoin(message) {
+    console.info("handleResponseJoin, remoteUid: " + message.remoteUid);
+    remoteUserId = message.remoteUid;
+    // doOffer();
+}
+
+function handleRemotePeerLeave(message) {
+    console.info("handleRemotePeerLeave, remoteUid: " + message.remoteUid);
+    remoteVideo.srcObject = null;
+}
+
+function handleRemoteNewPeer(message) {
+    console.info("handleRemoteNewPeer, remoteUid: " + message.remoteUid);
+    remoteUserId = message.remoteUid;
+    doOffer();
+}
+
+function doOffer() {
+    // 创建RTCPeerConnection
+    if (pc == null) {
+        createPeerConnection();
+    }
+    pc.createOffer().then(createOfferAndSendMessage).catch(handleCreateOfferError);
+}
+
+function doJoin(roomId) {
+    var jsonMsg = {
+        'cmd': 'join',
+        'roomId': roomId,
+        'uid': localUserId,
+    }; 
+    var message = JSON.stringify(jsonMsg);
+    zeroRTCEngine.sendMessage(message);
+    console.info("doJoin message: " + message);
+}
+
+function doLeave() {
+    var jsonMsg = {
+        'cmd': 'leave',
+        'roomId': roomId,
+        'uid': localUserId,
     };
     var message = JSON.stringify(jsonMsg);
-    ZeroRTCEngine.sendMsg(msg);
-    console.info("doJoin massage: " + msg);
+    zeroRTCEngine.sendMessage(message);
+    console.info("doLeave message: " + message);
 }
-
-function openLocalStream(stream){
+function openLocalStream(stream) {
     console.log('Open local stream');
+    doJoin(roomId);
     localVideo.srcObject = stream;
-    loaclStream = null;
+    localStream = stream;
 }
 
-function initLocalStream(){
+
+function initLocalStream() {
     navigator.mediaDevices.getUserMedia({
-        audio:true,
-        video:true
+        audio: true,
+        video: true
     })
     .then(openLocalStream)
-    .catch(function(e){
-        alert("getUserMedia() error:" + e.name)
+    .catch(function(e) {
+        alert("getUserMedia() error: " + e.name);
     });
-
 }
 
-ZeroRTCEngine = new ZeroRTCEngine("ws://192.168.221.134:8099");
-ZeroRTCEngine.createWebsocket();
+zeroRTCEngine = new ZeroRTCEngine("ws://10.23.57.12:8099");
+zeroRTCEngine.createWebsocket();
 
-document.getElementById('joinBottom').onclick = function(){
-    roomID = document.getElementById('roomID').value;
-    console.log("加入按钮已点击，roomID: " + roomID);
-    //初始化本地码流
+document.getElementById('joinBtn').onclick = function() {
+    roomId = document.getElementById('zero-roomId').value;
+    if( roomId == "" || roomId == "请输入房间ID") {
+        alert("请输入房间ID");
+        return;
+    }
+    console.log("加入按钮被点击, roomId: " + roomId);
+    // 初始化本地码流
     initLocalStream();
 }
 
-
+document.getElementById('leaveBtn').onclick = function() {
+    console.log("离开按钮被点击");
+    doLeave();
+}
 
